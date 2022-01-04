242f5e552bf8665edb0a36a81ebf1cd7fd26a1fc
rryaboshtan <rrvworkroman@gmail.com>
2022-01-04 17:17:14 +0200

    change server.js

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ if (process.env.NODE_ENV === 'production') {
    });
 }
 
-app.listen(5000, () => console.log('Server is started on http://localhost:5000'));
+app.listen(process.env.PORT || 5000, () => console.log('Server is started on http://localhost:5000'));
