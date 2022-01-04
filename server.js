cd4235e7e26c00fcd9f9ef67974c039565856466
rryaboshtan <rrvworkroman@gmail.com>
2022-01-04 17:11:43 +0200

    change server.js

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+
+// const whitelist = ['http://localhost:3000', 'http://localhost:5000', 'https://googlemapsapplication.herokuapp.com'];
+// const corsOptions = {
+//    origin: function (origin, callback) {
+//       console.log('** Origin of request ' + origin);
+//       if (whitelist.indexOf(origin) !== -1 || !origin) {
+//          console.log('Origin acceptable');
+//          callback(null, true);
+//       } else {
+//          console.log('Origin rejected');
+//          callback(new Error('Not allowed by CORS'));
+//       }
+//    },
+// };
+
+
 app.use(cors());
 
 app.use(express.json());
@@ -11,5 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 require('./src/database/database')();
 
 app.use('/', require('./src/router/router'));
+
+// For deploy only
+if (process.env.NODE_ENV === 'production') {
+   // Serve any static files
+   app.use(express.static(path.join(__dirname, 'frontend/build')));
+   // Handle React routing, return all requests to React app
+   app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+   });
+}
 
 app.listen(5000, () => console.log('Server is started on http://localhost:5000'));
